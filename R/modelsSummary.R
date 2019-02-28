07857403c3f5d4d6e93d93cba902faee8b138c30
Keon-Woong Moon <cardiomoon@gmail.com>
2019-02-28 15:35:37 +0900

    0.1.0
    0.1.0

--- R/modelsSummary.R
+++ R/modelsSummary.R
@@ -139,13 +139,13 @@ centerPrint=function(string,width){
 #' @export
 #' @return A flextable
 #' @examples
-#' fit1=lm(mpg~wt,data=mtcars)
-#' fit2=lm(mpg~wt*hp,data=mtcars)
+#'# fit1=lm(mpg~wt,data=mtcars)
+#'# fit2=lm(mpg~wt*hp,data=mtcars)
 #'# fit3=lm(mpg~wt*hp*am,data=mtcars)
 #'# x=modelsSummary(list(fit1))
 #'# modelsSummaryTable(x)
-#' x=modelsSummary(list(fit1,fit2))
-#' modelsSummaryTable(x,vanilla=FALSE)
+#'# x=modelsSummary(list(fit1,fit2))
+#'# modelsSummaryTable(x,vanilla=FALSE)
 #'# x=modelsSummary(list(fit1,fit2,fit3))
 #'# modelsSummaryTable(x)
 modelsSummaryTable=function(x,vanilla=TRUE){
