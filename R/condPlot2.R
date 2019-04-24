8e76442dd87eda17111eea8721d37a9b1d0a3480
Keon-Woong Moon <cardiomoon@gmail.com>
2019-04-24 13:32:08 +0900

    0.1.9
    0.1.9

--- R/condPlot2.R
+++ R/condPlot2.R
@@ -1,20 +1,31 @@
 #' Draw conditional plot for moderated moderation
 #' @param fit An object of class lm
-#' @param mod2 name of moderator variable
+#' @param pred name of the predictor variable
+#' @param modx name of the moderator variable
+#' @param mod2 name of the second moderator variable
 #' @param mod2.values values od moderator variable
+#' @param vjust integer
 #' @param rangemode integer. 1 or 2
 #' @param digits integer indicating the number of decimal places
 #' @param addlabel logical
+#' @param xvar character. "Z" or "W"
+#' @param ... Further arguments to be passed to predict3d::ggPredict()
 #' @importFrom predict3d ggPredict
 #' @export
 #' @examples
 #' fit=lm(govact~negemot*sex*age+posemot+ideology,data=glbwarm)
 #' condPlot2(fit)
 #' condPlot2(fit,mod2.values = c(30,50,70))
-condPlot2=function(fit,mod2=NULL,mod2.values=NULL,rangemode=1,digits=3,addlabel=TRUE){
+#' fit1=lm(govact~negemot*age*sex+posemot+ideology,data=glbwarm)
+#' condPlot2(fit1,pred="negemot",modx="sex",mod2="age",mod2.values = c(30,50,70),xvar="W")
+condPlot2=function(fit,pred=NULL,modx=NULL,mod2=NULL,mod2.values=NULL,
+                   rangemode=1,vjust=NULL,digits=3,addlabel=TRUE,xvar="Z",...){
 
-    # mod2=NULL;mod2.values = c(30,50,70);rangemode=1;digits=3
 
+      # fit=lm(govact~negemot*age*sex+posemot+ideology,data=glbwarm)
+      # pred="negemot";modx="sex"; mod2="age";mod2.values =NULL;rangemode=1;digits=3
+    if(is.null(pred)) pred=names(fit$model)[2]
+    if(is.null(modx)) modx=names(fit$model)[3]
     if(is.null(mod2)) mod2=names(fit$model)[4]
     if(is.null(mod2.values)){
         if(rangemode==1){
@@ -26,12 +37,13 @@ condPlot2=function(fit,mod2=NULL,mod2.values=NULL,rangemode=1,digits=3,addlabel=
         }
     }
 
-    vjust=rep(c(-0.5,1.5),each=length(mod2.values))
-    res=ggPredict(fit,show.point=FALSE,mod2.values = mod2.values,
-                  vjust=vjust,digits=digits,plot=FALSE)
+    if(is.null(vjust)) vjust=rep(c(-0.5,1.5),each=length(mod2.values))
+    temp=paste0("ggPredict(fit,pred=",pred,",modx=",modx,",mod2=",mod2,",show.point=FALSE,mod2.values = mod2.values,
+                vjust=vjust,digits=digits,plot=FALSE,...)")
+    res=eval(parse(text=temp))
     info=res$aspectRatio
 
-    df=makeLabel(fit,mod2.values=mod2.values,digits=digits)
+    df=makeLabel(fit,pred=pred,modx=modx,mod2=mod2,mod2.values=mod2.values,xvar=xvar,digits=digits)
     p<- res$p + guides(colour=FALSE)
 
     if(addlabel){
@@ -43,21 +55,27 @@ condPlot2=function(fit,mod2=NULL,mod2.values=NULL,rangemode=1,digits=3,addlabel=
 
 #' Make Labels
 #' @param fit An object of class lm
+#' @param pred name of the predictor variable
+#' @param modx name of the moderator variable
+#' @param mod2 name of the second moderator variable
 #' @param mod2.values values od moderator variable
+#' @param xvar character. "Z" or "W"
 #' @param digits integer indicating the number of decimal places
 #' @export
-makeLabel=function(fit,mod2.values = c(30,50,70),digits=3){
+makeLabel=function(fit,pred,modx,mod2,mod2.values = c(30,50,70),xvar="Z",digits=3){
     temp=paste0(deparse(fit$call),collapse="")
-    varname=names(fit$model)[4]
-    varname
+    varname=mod2
+
     est=c()
     p=c()
     for(i in 1:length(mod2.values)){
         newvar=paste0("I(",varname,"-",mod2.values[i],")")
         temp1=stringr::str_replace(temp,varname,newvar)
         fit1=eval(parse(text=temp1))
-        est=c(est,summary(fit1)$coef[7,1])
-        p=c(p,summary(fit1)$coef[7,4])
+        select=which(names(fit1$coef)==paste0(pred,":",modx))
+        if(length(select)==0) select=which(names(fit1$coef)==paste0(modx,":",pred))
+        est=c(est,summary(fit1)$coef[select,1])
+        p=c(p,summary(fit1)$coef[select,4])
     }
     df=data.frame(mod2.values,est,p)
     names(df)[1]=varname
@@ -65,7 +83,11 @@ makeLabel=function(fit,mod2.values = c(30,50,70),digits=3){
     df[[3]]=myformat(df[[3]],digits=digits)
     df[[3]]=pformat(df[[3]])
     df[[3]]=ifelse(nchar(df[[3]])==4,paste0(" = ",df[[3]]),paste0(" ",df[[3]]))
-    df$label1=paste0("theta[italic(X)*italic(W)%->%italic(Y)] ==",df$est)
+    if(xvar=="Z"){
+       df$label1=paste0("theta[italic(X)*italic(W)%->%italic(Y)] ==",df$est)
+    } else{
+        df$label1=paste0("theta[italic(X)*italic(Z)%->%italic(Y)] ==",df$est)
+    }
     df$label2= paste0("italic(p),'",df$p,"'")
     df$label=paste0("paste(",df$label1,",', ',",df$label2,")")
     df[["mod2group"]]=paste0(varname," = ",df[[varname]])
--- R/modSummary2.R
+++ R/modSummary2.R
@@ -1,101 +1,60 @@
 #' Make table summarizing moderation effect
-#'@param fit An object of class lm
-#'@param pred name of predictor variable
-#'@param modx name of moderator variable
-#'@param pred.values Values of predictor variables
-#'@param modx.values Values of modifier variables
-#'@param rangemode integer. 1 or 2
-#'@param digits integer indicating the number of decimal places
-#'@export
-#'@examples
-#'fit=lm(justify~frame*skeptic,data=disaster)
-#'modSummary2(fit)
-modSummary2=function(fit,pred=NULL,modx=NULL,pred.values=NULL,modx.values=NULL,
-                    rangemode=2,digits=3){
-    data=fit$model
-
-    dep=colnames(data)[1]
-    if(is.null(pred)) pred=colnames(data)[2]
-    if(is.null(modx)) modx=colnames(data)[3]
-
-
-
+#' @param fit An object of class lm
+#' @param rangemode An integer. If 1, mean+c(-1,0,1)*sd used. If 2, 16th, 50th and 84th percentiles are used
+#' @param pred.values Values of predictor variables
+#' @param summarymode An integer. 1 or 2. SUmmarizing method of variables. If 1, typical values are used. If 2, mean values are used
+#' @param maxylev An integer. Maximum length of predictor variables to be treated as a categorical variable.
+#' @param digits An integer indicating the number of decimal places
+#' @param labels Optional list of labels of variables
+#' @param ... Further arguments to be passed to predict3d::fit2newdata()
+#' @importFrom predict3d fit2newdata
+#' @importFrom stringr str_detect
+#' @export
+#' @examples
+#' labels=list(X="negemot",W="sex",Z="age",Y="govact",C1="posemot",C2="ideology")
+#' fit=lm(govact~negemot*sex+negemot*age+posemot+ideology,data=glbwarm)
+#' modSummary2(fit,rangemode=2,mod2.values=c(30,50,70),summarymode=2)
+#' modSummary2(fit,mod2.values=c(30,50,70),summarymode=1,labels=labels)
+#' labels=list(X="frame",W="skeptic",Y="justify")
+#' fit=lm(justify~frame*skeptic,data=disaster)
+#' modSummary2(fit,labels=labels)
+modSummary2=function(fit,rangemode=2,pred.values=NULL,summarymode=2,maxylev=6,digits=3,labels=NULL,...){
+    # rangemode=2;pred.values=NULL;summarymode=2;maxylev=6;digits=3
+    temp=names(fit$coef)[str_detect(names(fit$coef),":")]
+    vars=unique(unlist(strsplit(temp,":")))
     if(is.null(pred.values)){
-        if(rangemode==1) {
-            pred.values=mean(data[[pred]],na.rm=TRUE)+c(-1,0,1)*sd(data[[pred]],na.rm=TRUE)
-        } else if(rangemode==2) {
-            pred.values=quantile(data[[pred]],probs=c(0.16,0.5,0.84),type=6)
-        }
-    }
-
-    if(is.null(modx.values)){
-        if(length(unique(data[[modx]]))<6) {
-            modx.values=unique(data[[modx]])
-        } else if(rangemode==1){
-            modx.values=mean(data[[modx]],na.rm=T)+c(-1,0,1)*sd(data[[modx]],na.rm=T)
+        x=fit$model[[vars[1]]]
+        if(length(unique(x))<=maxylev){
+            pred.values=sort(unique(x))
+        } else if(rangemode==1) {
+            pred.values=mean(x,na.rm=TRUE)+c(-1,0,1)*sd(x,na.rm=TRUE)
         } else{
-            modx.values=quantile(data[[modx]],probs=c(0.16,0.5,0.84),type=6)
+            pred.values=quantile(x,probs=c(0.16,0.50,0.84),type=6)
         }
     }
+    df=fit2newdata(fit,predictors=vars,mode=rangemode,pred.values=pred.values,
+                summarymode=summarymode,...)
+    # df=fit2newdata(fit,predictors=vars,mode=rangemode,pred.values=pred.values,
+                 # summarymode=summarymode)
 
-    df1=calEquation(fit,modx.values = modx.values)
-    df1[[modx]]=modx.values
-
-    df=tidyr::crossing(pred.values,modx.values)
-    names(df)=c(pred,modx)
-
-    df<-dplyr::left_join(df,df1)
-
-    df[[dep]]=df$slope*df[[pred]]+df$intercept
-
-    df<-df[c(pred,modx,dep)]
-    colnames(df)=c(paste0(pred,"(X)"),paste0(modx,("(W)")),"\u0176")
-    df[]=lapply(df,myformat,digits)
-    coef=round(fit$coef,digits)
-
-    temp=paste0("\u0176 = ",coef[1], coef2str(coef[pred]),"X",
-                coef2str(coef[modx]),"W",coef2str(coef[paste0(pred,":",modx)]),"XW")
-    attr(df,"eq")=temp
-    slope=paste0("\u03F4x\u2192y = ",coef[pred],coef2str(coef[paste0(pred,":",modx)]),"W")
-    attr(df,"slope")=slope
-    class(df)=c("modSummary2","data.frame")
+    df[]=lapply(df,myformat,digits=digits)
+    if(!is.null(labels)) {
+        finalNames=changeLabelName(colnames(df),labels,add=TRUE)
+        colnames(df)=finalNames
+    }
     df
 }
-
-#'coonvert number to signed string
-#'@param x A number
-#'@export
-coef2str=function(x){
-    paste0(ifelse(sign(x)>=0," + "," - "),abs(x))
-}
-
-#'S3 method of print
-#'@param x An object of class modSummary2
-#'@param ... Further argument to be passed to print()
-#'@export
-print.modSummary2=function(x,...){
-    cat("\nSummary of moderation effect\n\n")
-    cat(attr(x,"eq"),"\n")
-    cat(attr(x,"slope"),"\n\n")
-    class(x)="data.frame"
-    print(x)
-}
-
 
 #' Make flextable summarizing moderation effect
 #' @param x An object
 #' @param vanilla logical
-#' @param ... Further argument to be passed to modSummary
+#' @param ... Further argument to be passed to modSummary3
 #' @export
-#' @importFrom flextable add_footer_lines align
-#'@examples
-#'fit=lm(justify~skeptic*frame,data=disaster)
-#'modSummary2Table(fit)
+#' @examples
+#' fit=lm(govact~negemot*sex+negemot*age+posemot+ideology,data=glbwarm)
+#' modSummary2Table(fit)
 modSummary2Table=function(x,vanilla=TRUE,...){
     if("lm" %in% class(x)) x=modSummary2(x,...)
-    rrtable::df2flextable(x,vanilla=vanilla) %>%
-        add_footer_lines(attr(x,"eq")) %>%
-        align(align="right",part="footer")
+    rrtable::df2flextable(x,vanilla=vanilla)
 }
-
 
