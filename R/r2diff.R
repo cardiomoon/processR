6d19dac9fba17c2bcff407810061947f5b2fd472
Keon-Woong Moon <cardiomoon@gmail.com>
2019-04-25 16:54:06 +0900

    0.1.9.1
    0.1.9.1

---
+++ R/r2diff.R
@@ -0,0 +1,51 @@
+#' Calculate difference of R2 and adjusted R2
+#' @param fit An object of class lm
+#' @param interaction.only Logical
+#' @param digits Integer indicating the number of decimal places
+#' @importFrom stringr str_detect
+#' @importFrom stats anova
+#' @export
+#' @examples
+#' fit=lm(mpg~wt*hp,data=mtcars)
+#' r2diff(fit)
+r2diff=function(fit,interaction.only=TRUE,digits=NULL){
+    fit.r2=summary(fit)$r.squared
+    fit.adj.r2=summary(fit)$adj.r.squared
+    vars=names(fit$coef)[-1]
+    df=fit$model
+    r2=c()
+    adj.r2=c()
+    f=c()
+    df1=c()
+    df2=c()
+    p=c()
+    for(i in 1:length(vars)){
+        temp=vars[-i]
+        eq=paste0(names(df)[1],"~",paste0(temp,collapse="+"))
+        fit1=lm(as.formula(eq),data=df)
+        r2=c(r2,summary(fit1)$r.squared)
+        adj.r2=c(adj.r2,summary(fit1)$adj.r.squared)
+        result=anova(fit1,fit)
+        f=c(f,result$F[2])
+        df1=c(df1,result$Df[2])
+        df2=c(df2,summary(fit)$df[2])
+        p=c(p,result$`Pr(>F)`[2])
+
+    }
+    r2diff=fit.r2-r2
+    ar2diff=fit.adj.r2-adj.r2
+    df=data.frame(r2diff=r2diff,ar2diff=ar2diff,F=f,df1=df1,df2=df2,p=p)
+    rownames(df)=vars
+    if(!is.null(digits)){
+    for(i in c(1,2,3,6)){
+      df[[i]]=myformat(df[[i]],digits)
+    }
+    df[[6]]=pformat(df[[6]])
+    }
+    if(interaction.only) {
+        select=stringr::str_detect(vars,":")
+        df=df[select,]
+    }
+    df
+}
+
